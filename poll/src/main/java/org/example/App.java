4cb8fb8613024b6abfe4018b4be8c8e0aaa1c684
Pavel <p.al.trofimov@gmail.com>
2024-11-08 23:21:36 +0300

    added facade realisation

--- poll/src/main/java/org/example/App.java
+++ poll/src/main/java/org/example/App.java
@@ -1,95 +1,18 @@
 package org.example;
 
 import org.example.components.PollFillingData;
-import org.example.components.PollQuestion;
-import org.example.components.PollQuestionResponse;
-import org.example.proxy.PollAnalyzerProxy;
-import org.example.strategy.AnalyzeStrategy;
-import org.example.strategy.impl.FullCountStrategy;
-import org.example.strategy.impl.LeastFrequentAnswerStrategy;
-import org.example.strategy.impl.MostFrequentAnswerStrategy;
+import org.example.facade.PollLifecycleFacade;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 public class App {
     public static void main(String[] args) {
-        Poll poll = Poll.builder()
-                .withPollName("Programming Survey")
-                .yesNoPollQuestion("Are you programmer?")
-                .oneVariantPollQuestion("How many years of programming experience do y ou have?")
-                .withMaxAnswers(1)
-                .withAnswerVariant("0-1 years")
-                .withAnswerVariant("1-3 years")
-                .withAnswerVariant("3-5 years")
-                .withAnswerVariant("5+ years")
-                .and()
-                .oneVariantPollQuestion("What is your favorite programming language?")
-                .withMaxAnswers(1)
-                .withAnswerVariant("Java")
-                .withAnswerVariant("Python")
-                .withAnswerVariant("C++")
-                .and()
-                .notMandatoryQuestion("Do you like your job?")
-                .withMaxAnswers(1)
-                .withAnswerVariant("Who knows..")
-                .withAnswerVariant("Of course")
-                .and()
-                .pollQuestion("What are your strong qualities?")
-                .withMinAnswers(0)
-                .withMaxAnswers(5)
-                .withAnswerVariant("Leadership")
-                .withAnswerVariant("Teamwork")
-                .withAnswerVariant("Problem-solving")
-                .withAnswerVariant("Communication")
-                .withAnswerVariant("Creativity")
-                .and()
-                .build();
+        PollLifecycleFacade pollLifecycleFacade = new PollLifecycleFacade();
 
-        List<PollFillingData> pollFillingDataList = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
-            pollFillingDataList.add(generateRandomFillingData(poll));
-        }
+        Poll poll = pollLifecycleFacade.createPoll();
 
-        AnalyzeStrategy fullCountStrategy = new FullCountStrategy();
-        AnalyzeStrategy leastFrequentAnswerStrategy = new LeastFrequentAnswerStrategy();
-        AnalyzeStrategy mostFrequentAnswerStrategy = new MostFrequentAnswerStrategy();
+        List<PollFillingData> userResponses = pollLifecycleFacade.getUserResponses(poll);
 
-        PollAnalyzer delegateAnalyzer = new PollAnalyzer(fullCountStrategy);
-        Analyzer analyzer = new PollAnalyzerProxy(delegateAnalyzer);
-
-        analyzer.analyzePoll(pollFillingDataList);
-
-        analyzer.changeAnalyzeStrategy(leastFrequentAnswerStrategy);
-        analyzer.analyzePoll(pollFillingDataList);
-
-        analyzer.changeAnalyzeStrategy(mostFrequentAnswerStrategy);
-        analyzer.analyzePoll(pollFillingDataList);
-
-        }
-
-    private static PollFillingData generateRandomFillingData(Poll poll) {
-        Random random = new Random();
-        List<PollQuestionResponse> responses = new ArrayList<>();
-
-        for (PollQuestion question : poll.getPollQuestionList()) {
-            int minAnswers = question.minAnswers();
-            int maxAnswers = question.maxAnswers();
-            List<String> answerVariants = question.answers();
-
-            int numberOfAnswers = minAnswers + random.nextInt(maxAnswers - minAnswers + 1);
-            List<String> selectedVariants = new ArrayList<>();
-
-            for (int i = 0; i < numberOfAnswers; i++) {
-                String randomVariant = answerVariants.get(random.nextInt(answerVariants.size()));
-                if(!selectedVariants.contains(randomVariant)) {
-                    selectedVariants.add(randomVariant);
-                }
-            }
-            responses.add(new PollQuestionResponse(question, selectedVariants));
-        }
-
-        return new PollFillingData("User №" + random.nextInt(), responses);
+        pollLifecycleFacade.makeAnalyzePoll(userResponses);
     }
 }
